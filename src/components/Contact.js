7a78c0e9d747f5a9af26611cf0acc61ed496d2d2
Aiman03-del <ausiaam83@gmail.com>
2024-06-04 23:55:01 +0600

    contact form connect to the mail

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,24 +1,17 @@
 import emailjs from "@emailjs/browser";
-import {
-  Box,
-  Button,
-  Card,
-  Container,
-  TextField,
-  Typography,
-} from "@mui/material";
+import { Box, Card, Container, TextField, Typography } from "@mui/material";
 import React, { useRef, useState } from "react";
 import styles from "./styles/Contact.module.css";
 
 export default function Contact() {
   const form = useRef();
-  const fileInput = useRef(); // Added a ref for file input
   const [fileSizeError, setFileSizeError] = useState("");
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    const file = fileInput.current.files[0]; // Retrieving file from ref
+    const fileInput = document.getElementById("file-upload");
+    const file = fileInput.files[0];
 
     if (file && file.size > 50000) {
       setFileSizeError("The file size exceeds the 50KB limit.");
@@ -29,62 +22,65 @@ export default function Contact() {
     const templateID = "template_o90z4br";
     const publicKey = "XZTT3FqzY8xg6Wz90";
 
-    console.log(form.current.attachment);
     const formData = new FormData(form.current);
-    // console.log(formData);
-    // formData.append("attachment", file);
-    console.log(formData.append("attachment", file));
-    emailjs.sendForm(serviceID, templateID, formData, publicKey).then(
+    formData.append("attachment", file);
+
+    // Check the formData content
+    // for (var pair of formData.entries()) {
+    //   console.log(pair[0] + ": " + pair[1]);
+    // }
+
+    emailjs.sendForm(serviceID, templateID, form.current, publicKey).then(
       (result) => {
         console.log("SUCCESS!", result.text);
-        setFileSizeError(""); // Clear error after successful submission
+        setFileSizeError("");
       },
       (error) => {
-        // console.log("FAILED...", error.text);
+        console.log("FAILED...", error.text);
       }
     );
   };
 
   return (
-    <Container
-      sx={{
-        maxWidth: { xs: "100%", md: 1000 },
-        marginTop: { xs: 5, md: 10 },
-        margin: "auto",
-        padding: { xs: 1, md: 2 },
-      }}
-    >
-      <Typography
-        sx={{ textAlign: "center", marginTop: 5 }}
-        gutterBottom
-        variant="h3"
-        component="div"
-      >
-        Contact <span className={styles.span}>Me?</span>
-      </Typography>
-      <Card
-        className={styles.contact}
+    <>
+      <Container
         sx={{
-          maxWidth: "100%",
-          width: { xs: "100%", sm: 500 },
+          maxWidth: { xs: "100%", md: 1000 },
+          marginTop: { xs: 5, md: 10 },
           margin: "auto",
-          marginTop: 10,
-          marginBottom: 3,
+          padding: { xs: 1, md: 2 },
         }}
-        variant="outlined"
       >
-        <form
-          className={styles.form} // Added a class for styling purposes
-          onSubmit={sendEmail}
-          ref={form}
+        <Typography
+          sx={{ textAlign: "center", marginTop: 5 }}
+          gutterBottom
+          variant="h3"
+          component="div"
+        >
+          Contact <span className={styles.span}>Me?</span>
+        </Typography>
+        <Card
+          className={styles.contact}
+          sx={{
+            maxWidth: "100%",
+            width: { xs: "100%", sm: 500 },
+            margin: "auto",
+            marginTop: 10,
+            marginBottom: 3,
+          }}
+          variant="outlined"
         >
           <Box
+            component="form"
             sx={{
               display: "flex",
               flexDirection: "column",
               gap: 2,
               marginTop: 4,
             }}
+            onSubmit={sendEmail}
+            ref={form}
+            encType="multipart/form-data"
           >
             <Container sx={{ marginTop: -2 }}>
               <div>
@@ -131,13 +127,10 @@ export default function Contact() {
               <div style={{ marginTop: 20 }}>
                 <label
                   htmlFor="file-upload"
-                  className={`${styles.customFileUpload} ${
-                    fileSizeError ? styles.hasError : ""
-                  }`}
+                  className={styles.customFileUpload}
                 >
                   <span> ATTACH FILE</span>
                   <input
-                    ref={fileInput}
                     id="file-upload"
                     type="file"
                     name="attachment"
@@ -150,13 +143,13 @@ export default function Contact() {
                   </Typography>
                 )}
               </div>
-              <Button className={styles.btn} type="submit">
+              <button className={styles.btn} type="submit">
                 Submit now
-              </Button>
+              </button>
             </Container>
           </Box>
-        </form>
-      </Card>
-    </Container>
+        </Card>
+      </Container>
+    </>
   );
 }
